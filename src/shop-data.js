0eca04f61a353b3a561b2e7012d2a04b81be0fbf
wangsamu <104201035+wangsamu@users.noreply.github.com>
2022-11-21 11:34:50 -0600

    New shop-data added

--- src/shop-data.js
+++ src/shop-data.js
@@ -18,221 +18,254 @@ const SHOP_DATA = [
       },
       {
         id: 3,
-        name: 'Brown Cowboy',
+        name: 'DURAFORCE-Triangle Ring- Squeakers',
         imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
         price: 35,
       },
       {
         id: 4,
-        name: 'Grey Brim',
-        imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
+        name: 'JW Pet Hol-ee Roller Dog Toy',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/67d/474/07e79acf2e0387a4097d7060fbaa8e86ed.2x.rsquare.w600.jpg',
         price: 25,
       },
       {
         id: 5,
-        name: 'Green Beanie',
-        imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
+        name: 'Happy Hands Happy Dogs Tug Toy',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/e75/fc8/7056e9e83bf3400cb26fa5c547bbd40fdb.2x.rdeep-vertical.w245.jpg',
         price: 18,
       },
       {
         id: 6,
-        name: 'Palm Tree Cap',
-        imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
+        name: 'Snuffle Nosework Dog Mat',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/132/53e/6925627a6ba36b7095a1cc8854dc0b8d0a.2x.rhorizontal.w600.jpg',
         price: 14,
       },
       {
         id: 7,
-        name: 'Red Beanie',
-        imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
+        name: 'AWOOF Pet Snuffle Mat for Dogs',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/b52/417/f1546081abb5ab1eacebe3f6644faaab56.2x.rsquare.w600.jpg',
         price: 18,
       },
       {
         id: 8,
-        name: 'Wolf Cap',
-        imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
+        name: 'Outward Hound Hide-A-Squirrel and Puzzle Plush, Jumbo',
+        imageUrl:
+          'https://www.amazon.com/dp/B005VS9WO6?tag=thestrategistsite-20&ascsubtag=[]st[p]cjh6kgxrr006k5iy6unpydtqq[i]zTvVOD[v]a[u]2[t]w[r]search.brave.com[d]D[z]m',
         price: 14,
       },
       {
         id: 9,
-        name: 'Blue Snapback',
-        imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
+        name: 'PetSafe Busy Buddy Twist ‘n Treat Dog Toy',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/016/4e8/5d7481546f67cdc8dd25a35e0eec00b7bb-8----.2x.rhorizontal.w600.jpg',
         price: 16,
       },
     ],
   },
   {
-    title: 'Sneakers',
+    title: 'Beds',
     items: [
       {
         id: 10,
-        name: 'Adidas NMD',
-        imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
+        name: 'Best Friends by Sheri Luxury Shag Faux-Fur Doughnut Cuddler $35 now 37% off',
+        imageUrl:
+          'https://www.amazon.com/dp/B01MV0IX66?tag=thestrategistsite-20&ascsubtag=[]st[p]cjh6kgxrr006k5iy6unpydtqq[i]onOJqB[v]a[u]2[t]w[r]search.brave.com[d]D[z]m',
         price: 220,
       },
       {
         id: 11,
-        name: 'Adidas Yeezy',
-        imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
-        price: 280,
+        name: 'Casper Memory-Foam Dog Bed',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/cb6/0ea/2ee4d7e0061b861e4a7d75ac3b3c5eac4c-casper.2x.rhorizontal.w600.jpg',
+        price: 220,
       },
       {
         id: 12,
-        name: 'Black Converse',
-        imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
-        price: 110,
-      },
-      {
-        id: 13,
-        name: 'Nike White AirForce',
-        imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
-        price: 160,
-      },
-      {
-        id: 14,
-        name: 'Nike Red High Tops',
-        imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
-        price: 160,
-      },
-      {
-        id: 15,
-        name: 'Nike Brown High Tops',
-        imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
-        price: 160,
-      },
-      {
-        id: 16,
-        name: 'Air Jordan Limited',
-        imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
-        price: 190,
-      },
-      {
-        id: 17,
-        name: 'Timberlands',
-        imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
-        price: 200,
+        name: 'Casper Memory-Foam Dog Bed',
+        imageUrl:
+          'https://go.redirectingat.com/?xs=1&id=1025X1552619&url=https%3A%2F%2Fwww.orvis.com%2Fp%2Forvis-comfortfill-eco-couch-dog-bed%2F20ff&xcust=___st__p_cjh6kgxrr006k5iy6unpydtqq__i_ziCJ20__v_a__u_2__t_w__r_search.brave.com__d_D__z_m&sref=https%3A%2F%2Fnymag.com%2Fstrategist%2Farticle%2Fbest-gifts-for-dog-lovers.html',
+        price: 220,
       },
     ],
   },
   {
-    title: 'Jackets',
+    title: 'Grooming',
     items: [
       {
-        id: 18,
-        name: 'Black Jean Shearling',
-        imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
+        id: 14,
+        name: 'KONG Dog ZoomGroom Multi-Use Brush - Pink',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/037/1ec/9402a3cf40c7b288868b66f9a2840e2979.2x.rdeep-vertical.w245.jpg',
         price: 125,
       },
       {
-        id: 19,
-        name: 'Blue Jean Jacket',
-        imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
-        price: 90,
+        id: 55,
+        name: 'Earthbath Oatmeal & Aloe Dog & Cat Shampoo (16 oz)',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/4cb/0e9/614ef8a496fa0f42977cff6c0a56cbff48.2x.rdeep-vertical.w245.jpg',
+        price: 125,
       },
       {
-        id: 20,
-        name: 'Grey Jean Jacket',
-        imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
-        price: 90,
-      },
-      {
-        id: 21,
-        name: 'Brown Shearling',
-        imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
-        price: 165,
-      },
-      {
-        id: 22,
-        name: 'Tan Trench',
-        imageUrl: 'https://i.ibb.co/M6hHc3F/brown-trench.png',
-        price: 185,
+        id: 56,
+        name: 'Aesop Animal Wash',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/6aa/6ed/09fe68d8610c99a599852bd0d1adf0cb51-aesop-01-.2x.rhorizontal.w600.jpg',
+        price: 125,
       },
     ],
   },
   {
-    title: 'Womens',
+    title: 'Feeders & Waterers',
     items: [
       {
-        id: 23,
-        name: 'Blue Tanktop',
-        imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
+        id: 15,
+        name: 'Neater Dog Feeder, Cappuccino',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/a1a/34e/47406e7cfb6d1fa543dc49781fc56da057-24---.2x.rhorizontal.w600.jpg',
         price: 25,
       },
       {
-        id: 24,
-        name: 'Floral Blouse',
-        imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
-        price: 20,
+        id: 16,
+        name: 'Outward Hound Fun Feeder Slow Feed Interactive Bloat Stop Dog Bowl',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/6cf/1f0/e3a4df926b248c722ce0f3b6e40b30786c-Outward-Hound.2x.rhorizontal.w600.jpg',
+        price: 25,
       },
       {
-        id: 25,
-        name: 'Floral Dress',
-        imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
-        price: 80,
+        id: 17,
+        name: 'Highwave AutoDogMug',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/249/475/5389983d95cd6f0e1d1cf1763335ea9721-191112-01.2x.rhorizontal.w600.jpg',
+        price: 25,
       },
       {
-        id: 26,
-        name: 'Red Dots Dress',
-        imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
-        price: 80,
-      },
-      {
-        id: 27,
-        name: 'Striped Sweater',
-        imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
-        price: 45,
-      },
-      {
-        id: 28,
-        name: 'Yellow Track Suit',
-        imageUrl: 'https://i.ibb.co/v1cvwNf/yellow-track-suit.png',
-        price: 135,
-      },
-      {
-        id: 29,
-        name: 'White Blouse',
-        imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
-        price: 20,
+        id: 18,
+        name: 'COMSUN Collapsible Dog Bowl',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/8f6/76b/9942e6b37b216a64b844a642fcb437e865-dogbowls.2x.rhorizontal.w600.jpg',
+        price: 25,
       },
     ],
   },
   {
-    title: 'Mens',
+    title: 'Clothing',
+    items: [
+      {
+        id: 19,
+        name: 'Carhartt Chore Coat',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/7d0/fc6/ec9d8d45c77251afd2771926b9f94e0455-chorecoat.2x.rsquare.w600.png',
+        price: 325,
+      },
+      {
+        id: 20,
+        name: 'Little Beast Silver Linings Sweater',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/59c/5a1/f08c91bc06b8ac862f5fd899210cc805ba-sky-sweater.2x.rsquare.w600.jpg',
+        price: 325,
+      },
+      {
+        id: 21,
+        name: 'K9 Apparel Custom Dog Winter Coat',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/dff/7a2/ea8f57132479b28a56896d9550dbd50d49-wc-custom-burgundy-900x.2x.rhorizontal.w600.jpg',
+        price: 325,
+      },
+      {
+        id: 22,
+        name: 'KULE X Maxbone Reversible Puffer Coat',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/6b0/940/6b1d8d168435318f424388892e128c7aa0-kule-dog-coat.2x.rdeep-vertical.w245.jpg',
+        price: 325,
+      },
+      {
+        id: 23,
+        name: 'Hurtta Expedition Insulated Dog Parka',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/7b8/69f/8941e55119f4f65650241753622b81af62.2x.rhorizontal.w600.jpg',
+        price: 325,
+      },
+    ],
+  },
+  {
+    title: 'Nutrition',
+    items: [
+      {
+        id: 62,
+        name: 'Jiminy’s Cricket Peas & Sweet-Potato Dog Treats',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/055/909/d05091d09c27d6cf036583013cc6c10a9d-jiminys.2x.rhorizontal.w600.jpg',
+        price: 325,
+      },
+      {
+        id: 60,
+        name: 'Medterra Bacon Flavored CBD Calming Treats',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/b58/3af/1b30c307ef8913616921d298924200fca7-CBDtreats.2x.rdeep-vertical.w245.png',
+        price: 325,
+      },
+      {
+        id: 61,
+        name: 'Zesty Paws Calming Bites for Dogs',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/7d3/b88/7b4fbed905a6c1852c7c204f3264520885-dog-treats-05.2x.rhorizontal.w600.jpg',
+        price: 325,
+      },
+    ],
+  },
+  {
+    title: 'Collars & Leashes',
     items: [
       {
         id: 30,
-        name: 'Camo Down Vest',
-        imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
+        name: 'Found My Animal Adjustable Ombré Rope Dog Leash, 7 Feet',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/d24/b01/2562197833a265fdaadea97deb7a4a6833-found-my-animal-leash.2x.rhorizontal.w600.jpg',
         price: 325,
       },
       {
         id: 31,
-        name: 'Floral T-shirt',
-        imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
-        price: 20,
+        name: 'Jelly Bean No-Pull Harness in Spice',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/774/22b/22fb55cf9ab95302b2b7db881ff25ccdf6-harness.2x.rdeep-vertical.w245.jpg',
+        price: 325,
       },
       {
         id: 32,
-        name: 'Black & White Longsleeve',
-        imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
-        price: 25,
+        name: 'Nice Beast Costa Mesa Dog Collar',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/566/6ed/5eb19098d4cbc75e2c4c0ffb5d7198f4e4-costa-mesa.2x.rhorizontal.w600.jpg',
+        price: 325,
+      },
+    ],
+  },
+  {
+    title: 'Other',
+    items: [
+      {
+        id: 50,
+        name: 'Camo Down Vest',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/55f/6df/621aec7c7c646b39c27084c056aeb7aa80-wild-one-poopbag.2x.rsquare.w215.jpg',
+        price: 325,
       },
       {
-        id: 33,
-        name: 'Pink T-shirt',
-        imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
-        price: 25,
+        id: 51,
+        name: 'Wild One Ecofriendly Poop Bags (60 Count)',
+        imageUrl:
+          'https://pyxis.nymag.com/v1/imgs/f02/40f/6ce67cbaff0306dc919204bab321f1a25b-SQUARE1.2x.rsquare.w215.jpg',
+        price: 325,
       },
       {
-        id: 34,
-        name: 'Jean Long Sleeve',
-        imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
-        price: 40,
-      },
-      {
-        id: 35,
-        name: 'Burgundy T-shirt',
-        imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
-        price: 25,
+        id: 52,
+        name: 'Trixie Portable Dog Splash Pool',
+        imageUrl:
+          'https://goto.walmart.com/c/1141873/565706/9383?u=https%3A%2F%2Fwww.walmart.com%2Fip%2F681932905&subId2=[]st[p]cjh6kgxrr006k5iy6unpydtqq[i]Drx6Em[v]a[u]2[t]w[r]search.brave.com[d]D[z]m',
+        price: 220,
       },
     ],
   },
